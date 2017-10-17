f67827f5b8ac360ee496e25616025a32b336c04c
Mike Wong <mikewongtkd@gmail.com>
2017-10-16 18:05:49 -0700

    Brackets now working.

--- trunk/frontend/html/include/brackets/js/brackets.js
+++ trunk/frontend/html/include/brackets/js/brackets.js
@@ -1,5 +1,5 @@
 $.widget( "freescore.brackets", {
-	options: { autoShow: true, height: '100%', width: '100%', stroke: 1 },
+	options: { autoShow: true, height: '100%', width: '100%', stroke: 3 },
 	_create: function() {
 		var o        = this.options;
 		var e        = this.options.elements = {};
@@ -23,12 +23,9 @@ $.widget( "freescore.brackets", {
 		var scale    = defined( o.scale ) ? o.scale : { height: 600, width: 240, match : { height: 80, width: 180 }};
 		var height   = m * (scale.height/4);
 		var width    = (n + 1) * scale.width;
-		var color    = defined( o.color ) ? o.color : { blue: '#286090', red: '#c9302c', white: 'white', current: '#f0ad4e', selected: '#5bc0de', line: '#ccc' };
+		var color    = defined( o.color ) ? o.color : { blue: '#286090', red: '#c9302c', white: 'white', black: 'black', current: '#f0ad4e', selected: '#5bc0de', line: '#666' };
 
 		draw.viewbox( 0, 0, width, height );
-
-		var selected = draw.rect( scale.match.width, scale.match.height ).attr({ id: 'bracket-selection' }).fill( 'none' ).stroke({ width: 20, color: color.selected }).radius( 6 ).hide();
-		var current  = draw.rect( scale.match.width, scale.match.height ).attr({ id: 'bracket-current'   }).fill( 'none' ).stroke({ width: 20, color: color.current  }).radius( 6 );
 
 		for( var j = 0; j < brackets.length; j++ ) {
 			var round = brackets[ j ];
@@ -38,14 +35,14 @@ $.widget( "freescore.brackets", {
 				var block    = scale.height/(4/Math.pow( 2, j ));
 				var y        = (i + 0.5) * block - 50;
 				var bracket  = round[ i ];
-				var blue     = { athlete : defined( bracket.blue.athlete ) ? athletes[ bracket.blue.athlete ] : { name: () => { return 'Bye' }}};
-				var red      = { athlete : defined( bracket.red.athlete )  ? athletes[ bracket.red.athlete ]  : { name: () => { return 'Bye' }}};
+				var blue     = { athlete : defined( bracket.blue.athlete ) ? athletes[ bracket.blue.athlete ] : { name: ( depth ) => { return depth == 0 ? 'Bye' : '' }}};
+				var red      = { athlete : defined( bracket.red.athlete )  ? athletes[ bracket.red.athlete ]  : { name: ( depth ) => { return depth == 0 ? 'Bye' : '' }}};
 				var id       = i + '-' + j + '-' + k;
 				var line     = { start: { x: x + scale.match.width, y: 0 }, head: { x: x + scale.match.width + 20, y: 0 }, foot: { x: x + scale.width - 20, y: y - (100 * j) - 60 }, stop: { x: x + scale.width, y: y - (100 * j) -60 }};
 				var match    = draw.group().attr({ id: id });
 
 				// ===== CLICK HANDLER
-				var clicked  = (function( id, x, y ) { var ijk = id.split( '-' ).map( function( x ) { return parseInt( x ); }); var clickEvent = { type: 'matchClicked', i: ijk[ 0 ], j: ijk[ 1 ], k: ijk[ 2 ]}; return function() { $( w ).trigger( clickEvent ); selected.move( x, y ); selected.show() }})( id, x, y );
+				var clicked  = (function( id, x, y ) { var ijk = id.split( '-' ).map( function( x ) { return parseInt( x ); }); var clickEvent = { type: 'matchClicked', i: ijk[ 0 ], j: ijk[ 1 ], k: ijk[ 2 ]}; return function() { $( w ).trigger( clickEvent ); }})( id, x, y );
 				
 				// ===== CALCULATE SCORES
 				blue.score   = bracket.blue.score;
@@ -54,16 +51,17 @@ $.widget( "freescore.brackets", {
 				blue.won     = defined( bracket.blue.athlete )   && (blue.score > red.score);
 				red.won      = defined( bracket.red.athlete )    && (red.score  > blue.score);
 
-				if( blue.won ) { line.start.y = y + 20; line.head.y = line.start.y; }
-				if( red.won  ) { line.start.y = y + 60; line.head.y = line.start.y; }
+				if( blue.won ) { line.start.y = y + 20; line.head.y = line.start.y; } else
+				if( red.won  ) { line.start.y = y + 60; line.head.y = line.start.y; } else
+							   { line.start.y = y + 40; line.head.y = line.start.y; }
 
 				// ===== RENDER THE MATCH
-				match.path('M 0 0 L 0 -30 Q 0 -40 10 -40 L 170 -40 Q 180 -40 180 -30 L 180 0 Z' ).fill( color.blue ).attr({ id: id + '-blue' }).move( 0,  0 );
-				match.path('M 0 0 L 0  30 Q 0  40 10  40 L 170  40 Q 180  40 180  30 L 180 0 Z' ).fill( color.red  ).attr({ id: id + '-red'  }).move( 0, 40 );
-				match.text( blue.athlete.name() ).font({ size: 24 }).fill( color.white ).move(  12,  8 );
-				match.text( red.athlete.name()  ).font({ size: 24 }).fill( color.white ).move(  12, 44 );
-				if( defined( bracket.blue.athlete )) { match.text( String( blue.score )).font({ size: 24 }).fill( color.white ).move( 160,  8 ); }
-				if( defined( bracket.red.athlete  )) { match.text( String( red.score  )).font({ size: 24 }).fill( color.white ).move( 160, 44 ); }
+				match.path('M 0 0 L 0 -30 Q 0 -40 10 -40 L 170 -40 Q 180 -40 180 -30 L 180 0 Z' ).stroke({ width: o.stroke, color: color.blue }).fill( color.white ).attr({ id: id + '-blue' }).move( 0,  0 - (o.stroke/2) );
+				match.path('M 0 0 L 0  30 Q 0  40 10  40 L 170  40 Q 180  40 180  30 L 180 0 Z' ).stroke({ width: o.stroke, color: color.red  }).fill( color.white ).attr({ id: id + '-red'  }).move( 0, 40 + (o.stroke/2) );
+				match.text( blue.athlete.name( j ) ).font({ size: 24 }).fill( color.black ).move(  12,  4 );
+				match.text( red.athlete.name( j )  ).font({ size: 24 }).fill( color.black ).move(  12, 48 );
+				// if( defined( bracket.blue.athlete )) { match.text( String( blue.score )).font({ size: 24 }).fill( color.black ).move( 160,  4 ); }
+				// if( defined( bracket.red.athlete  )) { match.text( String( red.score  )).font({ size: 24 }).fill( color.black ).move( 160, 48 ); }
 
 				match.move( x, y );
 				match.click( clicked ); // Apply click behavior
@@ -76,10 +74,6 @@ $.widget( "freescore.brackets", {
 					if( !( i % 2 )) { line.foot.y = (i + 1.0) * block      - 10; line.stop.y = line.foot.y; }
 					else            { line.foot.y =  i        * block      - 10; line.stop.y = line.foot.y; }
 					draw.polyline( poly( line )).fill( 'none' ).stroke({ width: o.stroke, color: color.line });
-				}
-
-				if( division.current.athleteId() == k ) {
-					current.move( x, y );
 				}
 
 				k++;
